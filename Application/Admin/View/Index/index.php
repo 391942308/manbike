a5b949487a4c84e535a38d4c3b8444dd80fb30fe
rabbit168 <844939525@qq.com>
2017-11-17 15:21:35 +0800

    新增柱状图

--- Application/Admin/View/Index/index.php
+++ Application/Admin/View/Index/index.php
@@ -35,19 +35,25 @@
               <!--<span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>-->
             </div>
           </div>
-
-<div id="main" style="width: 100%;height:600px;"></div>
-<div style="text-align:right;">
-<volist name="list1" id="vo">
-<a id="{$vo.keyword}" href="javascript:void(0);" type="button" class="btn btn-primary xx" >{$vo.title}</a>
-</volist>
-<!--
-<a id="xcj" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">摩拜</a>
-<a id="cx" href="#" type="button" class="btn btn-primary" >酷骑</a>
-<a id="cx" href="#" type="button" class="btn btn-primary" >hellobike</a>
-<a id="cx" href="#" type="button" class="btn btn-primary" >小明单车</a>
-<a id="cx" href="#" type="button" class="btn btn-primary" >蜜蜂出游</a>
-<a id="cx" href="#" type="button" class="btn btn-primary" >其他</a>-->
+<div>
+	<div style="float:left;width:60%;"> 
+	<volist name="list1" id="vo">
+	<a id="{$vo.keyword}" href="javascript:void(0);" type="button" class="btn btn-primary xx" >{$vo.title}</a>
+	</volist>
+	</div>
+	<div style="float:left;width:40%;">
+	<a id="day1" href="javascript:void(0);" type="button" class="btn btn-primary">当天</a>
+	<a id="day3" href="javascript:void(0);" type="button" class="btn btn-primary">3天</a>
+	<a id="day7" href="javascript:void(0);" type="button" class="btn btn-primary">7天</a>
+	<a id="day30" href="javascript:void(0);" type="button" class="btn btn-primary">30天</a>
+	</div>
+	<div style="clear:both;"></div>
+</div>
+<div>
+<div id="main" style="width:50%;height:500px;float:left;"></div>
+<div id="center" style="width:6%;height:500px;float:left;"></div>
+<div id="main1" style="width:40%;height:430px;float:left;"></div>
+<div style="clear:both"; > </div>
 </div>
 <!--可以隔一定时间记录一次，然后显示变化情况-->
  <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
@@ -63,7 +69,7 @@
 
 		option = {
 			title : {
-				text: '当前各个单车公司单车数量统计表',
+				text: '单车总量统计表',
 				//subtext: '纯属虚构',
 				x:'left'
 			},
@@ -91,7 +97,14 @@
 							shadowBlur: 10,
 							shadowOffsetX: 0,
 							shadowColor: 'rgba(0, 0, 0, 0.5)'
-						}
+						},
+						normal:{ 
+							label:{ 
+								show: true, 
+								formatter: '{b} :  \n ({d}%)' 
+							}, 
+							labelLine :{show:true} 
+						} 
 					}
 				}
 			]
@@ -101,18 +114,86 @@
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
 		
+		var jstr11 = $.parseJSON('{$str11}');
+		var jstr22 = $.parseJSON('{$str22}');
+		bar(jstr11,jstr22);
+		function bar(jstr11,jstr22){
+			// 基于准备好的dom，初始化echarts实例
+			var myChart1 = echarts.init(document.getElementById("main1"));
+			var app = {};
+			option = null;
+			option1 = {
+				title: {
+					text: '{$day}天新增量'
+				},
+				tooltip: {},
+				legend: {
+					data:['数量']
+				},
+				xAxis: {
+					type: 'category',  
+					boundaryGap: [0, 0.01],  
+					axisLabel:{
+						interval:0,
+						rotate:-30
+					},
+					data: jstr11
+				},
+				yAxis: {},
+				series: [{
+					name: '数量',
+					type: 'bar',
+					data: jstr22
+				}],
+				color:['#4876FF', 'green','yellow','blueviolet']
+			};;
+			
+			myChart1.setOption(option1, true);
+		}
+		
+		
+		
 		function realtime(){
 			location.reload();
 			console.log("realtime");
 		}
 		setInterval("realtime()",60000)
     </script>
+	
+	<script>
+	//ajax 获取到数据 ，然后显示
+		$("#day1").click(function(){
+			//alert("day1");
+			var url= 'http://116.62.171.54:8080/manbike0.3/index.php/Admin/Index/index'+'/day/1';
+			location.href=url;
+		})
+		$("#day3").click(function(){
+			//alert("day3");
+			var url= 'http://116.62.171.54:8080/manbike0.3/index.php/Admin/Index/index'+'/day/3';
+			location.href=url;
+		})
+		$("#day7").click(function(){
+			//alert("day7");
+			var url= 'http://116.62.171.54:8080/manbike0.3/index.php/Admin/Index/index'+'/day/7';
+			location.href=url;
+		})
+		$("#day30").click(function(){
+			//alert("day30");
+			var url= 'http://116.62.171.54:8080/manbike0.3/index.php/Admin/Index/index'+'/day/30';
+			location.href=url;
+		})
+	</script>
+	
+	
 	<script>
 	$(".xx").click(function(){
 		var id = $(this).attr("id");
 		var title=$(this).text();
 		//alert(id);
-		location.href="http://116.62.171.54:8080/manbike0.3/index.php/Admin/Bike/index"+"/name/"+id+"/company/"+title;
+		var url="http://116.62.171.54:8080/manbike0.3/index.php/Admin/Bike/index"+"/name/"+id+"/company/"+title;
+		window.open(url);
+		
+		
 	});
 	</script>
 

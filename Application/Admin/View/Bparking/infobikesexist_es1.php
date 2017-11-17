429be7121fdc7810457a809d86db81765e98d9e9
ougexingfuba <2845453773@qq.com>
2017-11-17 14:41:06 +0800

    车位管理，实时车位、历史车位饼图颜色修改视图

--- Application/Admin/View/Bparking/infobikesexist_es1.php
+++ Application/Admin/View/Bparking/infobikesexist_es1.php
@@ -20,20 +20,20 @@
 		location.reload();
 		console.log("realtime");
 	}
-	setInterval("realtime()",60000)
+	setInterval("realtime()",60000);
 	</script>
 	<script type="text/javascript">
-		
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
-//var test = genData(50);
-        
+
+		// 基于准备好的dom，初始化echarts实例
+		var myChart = echarts.init(document.getElementById('main'));
+		//var test = genData(50);
+
 		var jstr1 = $.parseJSON('{$str1}');
 		var jstr2 = $.parseJSON('{$str2}');
-
+		var jstr3 = $.parseJSON('{$str_color}');
 		option = {
 			title : {
-				text: '实时车位各个公司单车数量',
+				text: '车位历史各个单车公司单车数量统计表',
 				//subtext: '纯属虚构',
 				x:'left'
 			},
@@ -41,19 +41,20 @@
 				trigger: 'item',
 				formatter: "{a} <br/>{b} : {c} ({d}%)"
 			},
+			color:jstr3,
 			legend: {
 				type: 'scroll',
 				orient: 'vertical',
-				right: 10,
-				top: 20,
+				right: 60,
+				top: 80,
 				bottom: 20,
-				data: jstr1
+				data: jstr2
 			},
 			series : [
 				{
 					name: '车企',
 					type: 'pie',
-					radius : '80%',
+					radius : '55%',
 					center: ['40%', '50%'],
 					data: jstr2,
 					itemStyle: {
@@ -68,8 +69,14 @@
 		};
 
 
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
-    </script>
+		// 使用刚指定的配置项和数据显示图表。
+		myChart.setOption(option);
+
+		function realtime(){
+			location.reload();
+			console.log("realtime");
+		}
+		setInterval("realtime()",60000)
+	</script>
     <!-- /page content -->
 </block>

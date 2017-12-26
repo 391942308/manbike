713363ab3b915559cd5af488572598ad19f27028
rabbit168 <844939525@qq.com>
2017-12-26 10:18:58 +0800

    峰谷

--- Application/Admin/Controller/NoparkingController.class.php
+++ Application/Admin/Controller/NoparkingController.class.php
@@ -2,117 +2,228 @@
 namespace Admin\Controller;
 use Think\Controller;
 class NoparkingController extends CommonController {
+	public function _initialize(){
+		parent::_initialize();
+	}
 	public function index() {
+		
+		//var_dump($_REQUEST);
+		$start = $_REQUEST['start'];
+		$end = $_REQUEST['end'];
+		if($start=='' and $end == ''){
+			$start=mktime(0,0,0,date('m'),date('d')-7,date('Y'));
+			$end=mktime(0,0,0,date('m'),date('d')+1,date('Y'))-1;	
+		}else{
+			$start = strtotime($start);
+			$end = strtotime($end);
+		}
+		
+		$this->assign('start',date('Y-m-d H:i:s', $start));
+		$this->assign('end',date('Y-m-d H:i:s', $end));
 		//连接本地的 Redis 服务
 		$redis = new\ Redis();
 		$redis->connect('116.62.171.54', 8085);
 		//查询出所有违停车位的信息
-		$npinfo = M('info')->where("usable_num=0")->select();
-		$arr =array();
-		foreach($npinfo as $k=>$v){
-			$map_ag['dwz_info_id']=array( $v['id']);//获取违停车位id的值
-			//从redis获取到数据 ，然后使用图表形式展现
-			$id=$map_ag['dwz_info_id'][0];//违停车位ID
-			//从redis获取到数据 ，然后使用图表形式展现
-			$all = $redis->scard("infobikes:$id");
-			$all_list = $redis->smembers("infobikes:$id");
-			//var_dump($all_list);
-			foreach($all_list as $keys=>$vals){
-				$mac = str_replace(':','-',$vals);
-				//var_dump("infobike:$id:$mac");
-				$exist = $redis->scard("infobike:$id:$mac");
-				//var_dump($exist);
-				$arr_all[$keys]['mac']=$vals;
-				$arr_all[$keys]['num']=$exist;
-				$arr_all[$keys]['dwz_info_id']=$id;
-				//var_dump($arr_all);
+		$info = M('info')->field("id,title,province,city,area,usable_num,la,lb,lc")->select();
+		
+		//$start=mktime(0,0,0,date('m'),date('d')-7,date('Y'));
+		//$end=mktime(0,0,0,date('m'),date('d')+1,date('Y'))-1;
+		
+		//echo $start;
+		//echo $end;
+		
+		$start = $start *1000;
+		$end = $end *1000;
+		//echo $start;
+		//echo $end;
+		//exit;
+		$list = M("info_block")->select();
+		$infolist = array();
+		$i=0;
+		foreach($list as $k=>$v){
+			$arr = explode('|',$v['content']);
+			foreach($arr as $kk=>$vv){
+				$min=$this->chewei_sort_min($vv,$start,$end);
+				$max=$this->chewei_sort($vv,$start,$end,'desc');
+				$map['id']=array('eq',$vv);
+				$one = M("info")->where($map)->find();
+				if($one != null){
+					//$list[$k][$vv]['title']=$one['title'];
+					$infolist[$i]['id'] = $v['id'];
+					$infolist[$i]['btitle'] = $v['title'];
+					$infolist[$i]['title'] = $one['id'].'&nbsp;&nbsp;'.$one['title'];
+					$infolist[$i]['max'] = $max;
+					$infolist[$i]['min'] = $min;
+					$i++;
+				}
+				
 			}
 		}
-		$this->assign('all',$all);
-		$this->assign('all_list',$all_list);
-		$this->assign('arr_all',$arr_all);
-		//循环判断
-//		foreach($ags as $k=>$v){
-//			if(!$v){//判断是否为空（false）
-//				unset($ags[$k]);//删除
-//			}
-//		}
-//		foreach($ags as $value){
-//			foreach($value as $v){
-//				$arr2[]=$v;
-//			}
-//		}
-////销毁
-//		unset($ags,$value,$v);
-//		foreach($arr2 as $k=>&$v){
-//			$v["bikes"]=json_decode($v["bikes"],true);
-//			//var_dump($v["bikes"][0]["mac"]);
-//		}
-		//unset($v["bikes"]); // 最后取消掉引用
-		//var_dump($arr2);
-
-		//用车位编号iid_mac作为键
-		//mac相同的就叠加
-
-
-
-		//$this->assign("arr2",$arr2);
+		//var_dump($infolist);
+		$this->assign('infolist',$infolist);
+		
+		//$this->assign('info',$info);
 		$this->display();
 	}
-	/**
-	 * 添加菜单
-	 */
-	public function add(){
-		$data['title'] = $_POST['title'];
-		$data['usable_num'] = $_POST['usable_num'];
-		$data['storage_num'] = $_POST['storage_num'];
-		$data['overflow_num'] = $_POST['overflow_num'];
-		$data['no'] = $_POST['no'];
-		$data['block_no'] = $_POST['block_no'];
-		$lng_lat = $_POST['lng_lat'];
-		$data['lng']=strstr($lng_lat,',',true);
-		$data['lat']=ltrim(strstr($lng_lat,','), ",");
-		$data['tinyurl']=time();
-		$result=M('info')->add($data);
-		if ($result) {
-			$this->success('添加成功');
-		}else{
-			$this->error('添加失败');
-		}
-	}
-	/**
-	 * 修改菜单
-	 */
-	public function edit(){
-		$data['title'] = $_POST['title'];
-		$data['usable_num'] = $_POST['usable_num'];
-		$data['storage_num'] = $_POST['storage_num'];
-		$data['overflow_num'] = $_POST['overflow_num'];
-		$data['no'] = $_POST['no'];
-		$data['block_no'] = $_POST['block_no'];
-		$lng_lat = $_POST['lng_lat'];
-		$data['lng']=strstr($lng_lat,',',true);
-		$data['lat']=ltrim(strstr($lng_lat,','), ",");
-		$id = $_POST['id'];
-		$result=M('info')->where("id=$id")->save($data);
-		if ($result) {
-			$this->success('修改成功',U('Admin/Bparking/index'));
-		}else{
-			$this->error('修改失败');
-		}
-	}
+	
+	
+	private function chewei_sort_min($id,$start,$end){
+		$lpath =  THINK_PATH.'Library/Vendor/vendor/autoload.php';
+		require $lpath;
+		$hosts = [
+			'116.62.171.54:8081',         // IP + Port
+		];
+		$client = \Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
+		//获取es最后更新的时间,在更新的时候使用
 
-	/**
-	 * 删除菜单
-	 */
-	public function delete(){
-		$id=$_GET["id"];
-		$result=M('info')->where("id=$id")->delete();
-		if($result){
-			$this->success('删除成功',U('Admin/Bparking/index'));
-		}else{
-			$this->error('删除失败');
-		}
+		$json = '{
+			  "version": true,
+			  "size": 1,
+			  "sort": [
+				{
+				  "storage_num": {
+					"order": "asc",
+					"unmapped_type": "boolean"
+				  }
+				}
+			  ],
+			  "query": {
+				"bool": {
+				  "must": [
+					{
+					  "query_string": {
+						"query": "dwz_info_id:'.$id.'",
+						"analyze_wildcard": true
+					  }
+					},
+					{
+					  "range": {
+						"timestamp": {
+						  "gte": '.$start.',
+						  "lte": '.$end.',
+						  "format": "epoch_millis"
+						}
+					  }
+					}
+				  ],
+				  "must_not": [
+					{
+					  "match_phrase": {
+						"storage_num": {
+						  "query": 0
+						}
+					  }
+					},
+					{
+					  "match_phrase": {
+						"storage_num": {
+						  "query": 1
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+			}';
+		$params = [
+				'index' => 'bike_index_v6',
+				'type' => 'dbs_realtime',
+				'body' => $json
+		];
+
+		$results = $client->search($params);
+		if($results['hits']['total'] == 0) return 0;
+		
+		$bikes  = $results['hits']['hits'][0]['_source']['bikes'];
+		//获取到的记录做解析
+		$bikes = json_decode($bikes);
+		
+		
+		return $results['hits']['hits'][0]['_source']['storage_num'];
+	}
+	
+	
+	private function chewei_sort($id,$start,$end,$sort){
+		$lpath =  THINK_PATH.'Library/Vendor/vendor/autoload.php';
+		require $lpath;
+		$hosts = [
+			'116.62.171.54:8081',         // IP + Port
+		];
+		$client = \Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
+		//获取es最后更新的时间,在更新的时候使用
+
+		$json = '{
+			  "version": true,
+			  "size": 1,
+			  "sort": [
+				{
+				  "storage_num": {
+					"order": "desc",
+					"unmapped_type": "boolean"
+				  }
+				}
+			  ],
+			  "query": {
+				"bool": {
+				  "must": [
+					{
+					  "query_string": {
+						"analyze_wildcard": true,
+						"query": "dwz_info_id:'.$id.'"
+					  }
+					},
+					{
+					  "range": {
+						"timestamp": {
+						  "gte": '.$start.',
+						  "lte": '.$end.',
+						  "format": "epoch_millis"
+						}
+					  }
+					},
+					{
+					  "range": {
+						"storage_num": {
+						  "lte": 1000
+						}
+					  }
+					}
+				  ],
+				  "must_not": [
+					{
+					  "match_phrase": {
+						"storage_num": {
+						  "query": 0
+						}
+					  }
+					},
+					{
+					  "match_phrase": {
+						"storage_num": {
+						  "query": 1
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+			}';
+		$params = [
+				'index' => 'bike_index_v6',
+				'type' => 'dbs_realtime',
+				'body' => $json
+		];
+
+		$results = $client->search($params);
+		
+		if($results['hits']['total'] == 0) return 0;
+		
+		$bikes  = $results['hits']['hits'][0]['_source']['bikes'];
+		//获取到的记录做解析
+		$bikes = json_decode($bikes);
+		
+		
+		return $results['hits']['hits'][0]['_source']['storage_num'];
 	}
 
 
